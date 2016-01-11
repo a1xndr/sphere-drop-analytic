f126c88169815a06fee6e407d0daaead60e510d3
Alexander Oleinik <alxndr@solo.vrmnet>
2016-01-11 15:17:29 -0500

    parallelized the batch job

--- sphere-drop-analytical/experiment/batch.sh
+++ sphere-drop-analytical/experiment/batch.sh
@@ -3,6 +3,7 @@ for x in 4 3 2 1 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1
 do
 for y in {1..5}
 do
-    ./sphere-drop-analytical 10 10 10 10 $x $x
+    ./sphere-drop-analytical 10 10 10 10 $x $x &
 done
+wait
 done
